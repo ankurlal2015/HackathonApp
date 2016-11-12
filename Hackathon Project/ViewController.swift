e2f64c5fa78580dab293009aed9000eddfe20637
Ankur Lal <ankur.lal@vanderbilt.edu>
2016-11-12 11:46:48 -0600

    Main home page buttons placed

--- Hackathon Project/ViewController.swift
+++ Hackathon Project/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBOutlet weak var NewAlarm: UIButton!
+    
+    @IBOutlet weak var MyAlarms: UIButton!
+    
+    
 
 }
 
